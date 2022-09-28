dbdb61935380b1dc6bd10e5b5dffe0f66279ab1a
cheakf <1442450941@qq.com>
2022-09-29 02:12:33 +0800

    我暂时放弃了，这c++截个屏对新手而言还是有难度啊

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,48 @@
-#include<algorithm>
+#include<iostream>
 #include<thread>
-//opencv
-#include<opencv2/opencv.hpp>
-#include<opencv2/core.hpp>
-#include<opencv2/highgui.hpp>
-#include<opencv2/imgproc.hpp>
+//Qt
+#include <QApplication>
+#include <QTextCodec>
+#include <QIcon>
 //导入自己写的头文件
-#include"utils.h"
+#include"utils.hpp"
+#include"Screen.hpp"
 
 void thread_print(){
     helloworld_print();
     midi_print();
 }
 
-void thread_screen(){
 
-}
+// int main(){
+//     std::thread t1_p(thread_print);
+//     std::thread t2_s(thread_screen);
+//     t1_p.join();
+//     t2_s.join();
+//     std::cout << "hello world" << std::endl;
+//     return 0;
+// }
 
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    a.setFont(QFont("Microsoft Yahei", 9));
 
-int main(){
-    std::thread t1_p(thread_print);
-    std::thread t2_s(thread_screen);
-    t1_p.join();
-    t2_s.join();
-    std::cout << "hello world" << std::endl;
-    return 0;
-}
+    #if (QT_VERSION <= QT_VERSION_CHECK(5,0,0))
+    #if _MSC_VER
+        QTextCodec *codec = QTextCodec::codecForName("gbk");
+    #else
+        QTextCodec *codec = QTextCodec::codecForName("utf-8");
+    #endif
+        QTextCodec::setCodecForLocale(codec);
+        QTextCodec::setCodecForCStrings(codec);
+        QTextCodec::setCodecForTr(codec);
+    #else
+        QTextCodec *codec = QTextCodec::codecForName("utf-8");
+        QTextCodec::setCodecForLocale(codec);
+    #endif
 
+    ScreenWidget::Instance()->showFullScreen();   //直接调用实例
+
+    return a.exec();
+} 
--- src/utils.cpp
+++ src/utils.cpp
@@ -1,4 +1,4 @@
-#include"utils.h"
+#include"utils.hpp"
 
 
 void helloworld_print(){
