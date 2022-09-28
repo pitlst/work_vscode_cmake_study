ddbf6dd0757a4c82fb316dd350968112162e2acb
cheakf <1442450941@qq.com>
2022-09-28 15:32:18 +0800

    首次提交

---
+++ include/utils.h
@@ -0,0 +1,9 @@
+//fmt
+#include<fmt/core.h>
+#include<fmt/format.h>
+#include<fmt/std.h>
+#include<fmt/os.h>
+#include<fmt/ostream.h>
+#include<fmt/color.h>
+
+void world_print();
---
+++ src/main.cpp
@@ -0,0 +1,20 @@
+#include<algorithm>
+#include<string>
+//opencv
+#include<opencv2/core.hpp>
+#include<opencv2/highgui.hpp>
+#include<opencv2/imgproc.hpp>
+//fmt
+#include<fmt/core.h>
+#include<fmt/format.h>
+#include<fmt/std.h>
+#include<fmt/os.h>
+#include<fmt/ostream.h>
+#include<fmt/color.h>
+//导入私人第三方的头文件
+#include"utils.h"
+
+int main(){
+    world_print();
+    return 0;
+}
